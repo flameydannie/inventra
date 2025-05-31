74c23ba9552a93e6857bfe6a4b966774e9a896f4
Kalutu <kalutudaniel@gmail.com>
2025-06-01 02:01:48 +0300

    feat: stock management

--- inventra/inventra/doctype/stock_entry/stock_entry.js
+++ inventra/inventra/doctype/stock_entry/stock_entry.js
@@ -1,8 +1,41 @@
 // Copyright (c) 2025, Kalutu and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Stock Entry", {
-// 	refresh(frm) {
+frappe.ui.form.on("Stock Entry", {
+	entry_type(frm) {
+		update_valuation_rates(frm);
+	},
+	source_warehouse(frm) {
+		update_valuation_rates(frm);
+	},
+});
 
-// 	},
-// });
+frappe.ui.form.on("Stock Entry Item", {
+	item(frm, cdt, cdn) {
+		update_valuation_rates(frm);
+	},
+});
+
+function update_valuation_rates(frm) {
+	if (!frm.doc || frm.doc.entry_type === "Receipt") return;
+
+	const warehouse = frm.doc.source_warehouse;
+	if (!warehouse) return;
+
+	(frm.doc.items || []).forEach((row) => {
+		if (row.item) {
+			frappe.call({
+				method: "inventra.inventra.doctype.stock_entry.stock_entry.calculate_valuation_rate",
+				args: {
+					item: row.item,
+					warehouse: warehouse,
+				},
+				callback: function (r) {
+					if (r.message !== undefined) {
+						frappe.model.set_value(row.doctype, row.name, "valuation_rate", r.message);
+					}
+				},
+			});
+		}
+	});
+}
--- inventra/inventra/doctype/stock_entry/stock_entry.py
+++ inventra/inventra/doctype/stock_entry/stock_entry.py
@@ -1,9 +1,64 @@
 # Copyright (c) 2025, Kalutu and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class StockEntry(Document):
-	pass
+    def before_save(self):
+        if self.entry_type != "Receipt":
+            for item in self.items:
+                item.valuation_rate = calculate_valuation_rate(item.item, self.source_warehouse)
+
+    def on_submit(self):
+        self.create_stock_ledger_entries()
+
+    def on_cancel(self):
+        self.delete_stock_ledger_entries()
+
+    def create_stock_ledger_entries(self):
+        """Create stock ledger entries based on entry type."""
+        for item in self.items:
+            if self.entry_type == "Receipt":
+                self.make_sle(item.item, self.target_warehouse, item.qty, item.valuation_rate)
+
+            elif self.entry_type in ("Consume", "Transfer"):
+                self.make_sle(item.item, self.source_warehouse, -item.qty, item.valuation_rate)
+
+                if self.entry_type == "Transfer":
+                    self.make_sle(item.item, self.target_warehouse, item.qty, item.valuation_rate)
+
+    def make_sle(self, item, warehouse, qty, valuation_rate):
+        """Create a single stock ledger entry."""
+        frappe.get_doc({
+            "doctype": "Stock Ledger Entry",
+            "item": item,
+            "warehouse": warehouse,
+            "posting_date": self.posting_date,
+            "qty_change": qty,
+            "valuation_rate": valuation_rate,
+            "stock_entry_reference": self.name
+        }).insert()
+
+    def delete_stock_ledger_entries(self):
+        """Delete all stock ledger entries linked to this stock entry."""
+        frappe.db.delete("Stock Ledger Entry", {
+            "stock_entry_reference": self.name
+        })
+
+
+@frappe.whitelist()
+def calculate_valuation_rate(item, warehouse):
+    """Calculate average valuation rate for the given item and warehouse."""
+    data = frappe.db.sql("""
+        SELECT 
+            SUM(qty_change) AS total_qty,
+            SUM(qty_change * valuation_rate) AS total_value
+        FROM `tabStock Ledger Entry`
+        WHERE item = %s AND warehouse = %s
+    """, (item, warehouse), as_dict=True)[0]
+
+    total_qty = data.total_qty or 0
+    total_value = data.total_value or 0
+
+    return (total_value / total_qty) if total_qty else 0
